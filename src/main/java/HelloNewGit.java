fa8b61f1bdf915d4cc9af0dd66209d49f58fc9c1
gx <894713904@qq.com>
2019-11-22 15:47:12 +0800

    这是我在idea上进行的第一次提交HelloNewGit文件

--- src/main/java/HelloNewGit.java
+++ src/main/java/HelloNewGit.java
@@ -10,5 +10,6 @@ public class HelloNewGit {
     public static void main(String[] args) {
         System.out.println("你好Kobe");
         System.out.println("他们到底在做些啥好玩的，我也不知道，好像他们很强啊");
+        System.out.println("又有新的变动了");
     }
 }

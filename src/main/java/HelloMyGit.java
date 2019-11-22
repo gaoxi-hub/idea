f3c73a0db2928be85c7bdbb3810597dd2dcbba53
gx <894713904@qq.com>
2019-11-22 15:33:52 +0800

    这是我在idea上进行的第一次提交HelloNewGit文件

---
+++ src/main/java/HelloMyGit.java
@@ -0,0 +1,15 @@
+/**
+ * ClassName: HelloMyGit
+ * Description:
+ * date: 2019/11/22 14:41
+ *
+ * @author 高溪
+ * @since JDK 1.8
+ */
+public class HelloMyGit {
+    public static void main(String[] args) {
+        System.out.println("Hello My Baby This your Git");
+        System.out.println("This is My problem");
+        System.out.println("DDDDD");
+    }
+}
---
+++ src/main/java/HelloNewGit.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: HelloNewGit
+ * Description:
+ * date: 2019/11/22 15:03
+ *
+ * @author 高溪
+ * @since JDK 1.8
+ */
+public class HelloNewGit {
+    public static void main(String[] args) {
+        System.out.println("你好Kobe");
+    }
+}

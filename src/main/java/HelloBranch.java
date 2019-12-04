1ae9e7e100170f084597c1dbb7a7893ce8141fc7
gx <894713904@qq.com>
2019-12-04 20:12:45 +0800

    这次总可以提交push了吧

---
+++ src/main/java/HelloBranch.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: HelloBranch
+ * Description:
+ * date: 2019/12/4 18:28
+ *
+ * @author gaoxi
+ * @since JDK 1.8
+ */
+public class HelloBranch {
+    public static void main(String[] args) {
+        System.out.println("Hello My New Branch");
+    }
+}
